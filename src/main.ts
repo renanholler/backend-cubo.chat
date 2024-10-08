db33c22405d30a026a129173f08084641fe7bacd
Renan Holler <rrholler@gmail.com>
2024-10-08 14:30:28 -0300

    adapting to deploy

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Configuração detalhada de CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // Permitir apenas esta origem
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     methods: 'GET,PATCH', // Métodos permitidos
     credentials: true, // Habilitar cookies
   });
